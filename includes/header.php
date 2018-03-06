4b807e5d51f02de41c8a8bb74735b008f2b413bc
thomas <brownaformac@gmail.com>
2018-03-06 11:27:47 +0100

    sass

--- includes/header.php
+++ includes/header.php
@@ -1,5 +1,5 @@
 <meta charset="utf-8">
 <script defer src="https://use.fontawesome.com/releases/v5.0.7/js/all.js"></script>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
-<link rel="stylesheet" href="css/style.css">
+<link rel="stylesheet" href="css/style1.css">
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
